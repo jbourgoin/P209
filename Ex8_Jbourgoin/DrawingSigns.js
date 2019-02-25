282ffa5cdc479875d25afbb4a2ffd784e4f7e214
joseph bourgoin <jabourg@gmail.com>
2019-02-25 13:38:09 -0800

    adding files for submission

--- Ex8_Jbourgoin/DrawingSigns.js
+++ Ex8_Jbourgoin/DrawingSigns.js
@@ -19,7 +19,7 @@ ctx.closePath();
 ctx.fill();
 ctx.stroke();	
 
-
+ctx.strokeStyle = "black";
 //straight line
 ctx.moveTo(370,255);
 ctx.lineTo(370,560);
@@ -32,5 +32,44 @@ ctx.lineTo(260,450);
 ctx.lineTo(310,355);
 ctx.lineTo(310,255);
 
+ctx.restore();
+
 ctx.stroke();
-		// restore previous display state
+
+var canvas = document.querySelector("#myCanvas2");
+var ctx2 = canvas.getContext("2d");
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx2.save();			// save previous display state
+//  set drawing properties for the sign
+ctx2.lineWidth = 32;	       // nice wide line		
+ctx2.lineJoin = "round";	   // rounded corners
+ctx2.strokeStyle = "red";
+ctx2.fillStyle = "red";
+
+// create octagon linear path
+ctx2.beginPath();
+ctx2.arc(300,300,100,0,2*Math.PI)
+ctx2.closePath();
+
+// fill the sign and draw wide red lines
+ctx2.fill();
+ctx2.stroke();	
+
+// draw narrower white lines -- these will display on top of the wide red lines and make the red lines
+// look like the outside edge -- a nice trick!
+ctx.save();
+// draw DO NOT ENTER text
+ctx2.fillStyle  = "white";
+
+ctx2.font = "bold 40px Arial";
+ctx2.fillText( "DO NOT" ,220, 290);
+ctx.strokeStyle = "white";
+ctx2.moveTo(220,320);
+ctx2.lineTo(320,220);
+ctx2.fillText( "ENTER" ,235, 360);
+ctx2.save();
+ctx2.restore();		// restore previous display state
+
